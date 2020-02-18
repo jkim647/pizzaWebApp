99886edd7aa29f024f1e151c126f8f984672f70b
Jungsup Kim <kim8299270@gmail.com>
2020-02-19 12:16:55 +1300

    comit

---
+++ pizza/src/Header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return(
+    <div className="slide">
+        <ul>
+            <li>ss</li>
+            <li>ss</li>
+        </ul>
+    </div>
+
+    )
+}
+
+export default Header
