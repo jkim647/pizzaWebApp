c71e0be9fa67287841b4157493ab18b39a7cb6f6
Jungsup Kim <kim8299270@gmail.com>
2020-02-19 16:23:23 +1300

    push

--- pizza/src/Header/header.js
+++ pizza/src/Header/header.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import pic_1 from '../img/pizza_header_3.jpg'
+import pic_2 from '../img/pizza_header_2.jpg'
 import './header.css';
+import Carousel from 'react-bootstrap/Carousel'
+
 
 const Header = () => {
     return(
-    <div className="slide">
-        <ul>
-            <li>ss</li>
-            <li>ss</li>
-        </ul>
-    </div>
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={pic_1}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={pic_2}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  
+</Carousel>
 
     )
 }
